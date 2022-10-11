c178a276865075d7c76f60aa7848944e705ad95b
taqnar <muzammilthottathil.official@gmail.com>
2022-10-11 19:51:37 +0530

    download brochure

--- src/pages/courses/index.jsx
+++ src/pages/courses/index.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { FaPlay } from "react-icons/fa";
 import { IoStar } from "react-icons/io5";
 import { FiChevronUp } from "react-icons/fi";
+import { HiOutlineDownload } from "react-icons/hi";
 import CrackedSdtSlider from "../../components/courses/CrackedSdtSlider";
 import Accordian from "../../components/courses/Accordian";
 import AdmissionProcess from "../../components/courses/AdmissionProcess";
@@ -905,6 +906,17 @@ export default function Courses() {
                                 </div>
                             ))}
                         </div>
+                    </div>
+                </section>
+
+                {/* Download Brochure */}
+                <section className="max-w-7xl mx-auto px-4 mt-8 md:mt-[4.5rem]">
+                    <div className="px-8 py-8 md:py-9 md:px-12 rounded-2xl w-full bg-[#00253A] flex flex-col md:flex-row items-center justify-center md:justify-between gap-5 md:gap-8">
+                        <p className="text-center md:text-left md:max-w-md text-white text-sm font-light">{`Learning powered with right ecosystem and activities that make you industry capabl. Download the Brochure for details!`}</p>
+                        <button className="bg-capabl_primary flex-shrink-0 flex items-center justify-center gap-2 rounded-lg px-4 py-2.5 text-sm transition-all duration-300 border border-transparent hover:border-capabl_primary hover:text-capabl_primary hover:bg-transparent">
+                            <HiOutlineDownload className="text-xl" />
+                            Download Brochure
+                        </button>
                     </div>
                 </section>
 
