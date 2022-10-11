bde85c7b896ef2e88c41818a92aae2132058c1be
taqnar <muzammilthottathil.official@gmail.com>
2022-10-11 19:38:51 +0530

    updated

---
+++ src/utils/data/courses/toolsAndSkills.js
@@ -0,0 +1,21 @@
+export const toolsLearnt = [
+    { image: "/images/courses/tableau.png", name: "Tableau" },
+    { image: "/images/courses/apache_spark.png", name: "Apache Spark" },
+    { image: "/images/courses/tableau.png", name: "Tableau" },
+    { image: "/images/courses/superset.png", name: "Superset" },
+    { image: "/images/courses/tableau.png", name: "Tableau" },
+    { image: "/images/courses/apache_spark.png", name: "Apache Spark" },
+    { image: "/images/courses/tableau.png", name: "Tableau" },
+    { image: "/images/courses/superset.png", name: "Superset" },
+];
+
+export const skillsAcquired = [
+    { image: "", name: "Skill 1" },
+    { image: "", name: "Skill 2" },
+    { image: "", name: "Skill 3" },
+    { image: "", name: "Skill 4" },
+    { image: "", name: "Skill 5" },
+    { image: "", name: "Skill 6" },
+    { image: "", name: "Skill 7" },
+    { image: "", name: "Skill 8" },
+];
