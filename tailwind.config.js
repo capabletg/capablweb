b4c4f639d7c031a9ff469c4c7aabcbe7d97bae43
taqnar <muzammilthottathil.official@gmail.com>
2022-12-22 07:25:48 +0530

    test commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,14 @@
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        capabl_primary: "#FFAF00",
-      },
-      boxShadow: {
-        "3xl": "0 35px 30px -9px rgba(0, 0, 0, 0.7)",
-      },
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                capabl_primary: "#FFCC00",
+            },
+            boxShadow: {
+                "3xl": "0 35px 30px -9px rgba(0, 0, 0, 0.7)",
+            },
+        },
     },
-  },
-  plugins: [
-    require("tailwind-scrollbar-hide"),
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/forms"),
-  ],
+    plugins: [require("tailwind-scrollbar-hide"), require("@tailwindcss/line-clamp"), require("@tailwindcss/forms")],
 };
