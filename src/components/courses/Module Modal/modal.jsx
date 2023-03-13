a418788106deb743d08252fbd7740384bdf47da6
zainabdulmuiz <116026841+zainabdulmuiz@users.noreply.github.com>
2023-03-13 08:46:28 +0530

    Update modal.jsx

--- src/components/courses/Module Modal/modal.jsx
+++ src/components/courses/Module Modal/modal.jsx
@@ -12,7 +12,7 @@ export default function Modal(moduleData = []) {
       {isOpen && (
         <>
           <div className="justify-center flex overflow-x-hidden overflow-y-hidden fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative min-w-[75%] my-4  mx-4  max-w-2xl scrollbar scrollbar-thumb-[#C1C1C1] scrollbar-track-gray-200">
+            <div className="relative min-w-[75%] my-4  mx-4  max-w-2xl overflow-auto scrollbar scrollbar-thumb-[#C1C1C1] scrollbar-track-gray-200">
               {/*content*/}
               <div className="  border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                 {/*header*/}
