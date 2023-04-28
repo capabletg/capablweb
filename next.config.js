495ce25323de6dd3ce50616530642d5ad290e2b7
Yash Agrawal <info@getraisetechnologies.com>
2023-04-28 13:33:33 +0530

    fix: fix for routing

--- next.config.js
+++ next.config.js
@@ -2,6 +2,20 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/join-now',
+        destination: 'https://rzp.io/l/join-now',
+        permanent: true
+      },
+      {
+        source: '/confirmtopay',
+        destination: 'https://rzp.io/l/confirmtopay',
+        permanent: true
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
